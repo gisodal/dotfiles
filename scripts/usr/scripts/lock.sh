4eee2028847f4006d2bdbec656ce203072f406af
gdal@clevo <gisodal@gmail.com>
2024-03-04 22:27:12 +0100

    updated lock script

--- scripts/usr/scripts/lock.sh
+++ scripts/usr/scripts/lock.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-#scrot /tmp/screen.png
-##convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
-#convert /tmp/screen.png -scale 10% -scale 1000% -fill black -colorize 25% /tmp/screen.png 
-#[[ -f $1 ]] && convert /tmp/screen.png $1 -gravity center -composite -matte /tmp/screen.png
-#i3lock -i /tmp/screen.png
-#rm /tmp/screen.png
-
-
 ICON=$HOME/.config/wallpaper/lock-icon-512.png
 IMAGE=/tmp/screen.png
 scrot $IMAGE
