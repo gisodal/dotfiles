3719f8e45873ae1f39c04bd2628a5833f706a317
gdal@clevo <gisodal@gmail.com>
2024-03-16 21:27:53 +0100

    easier session kill script; vim alias to nvim

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -1,8 +1,9 @@
 alias ls="command ls --color=auto -v -h --group-directories-first -a -I .. -I . -I .git"
 alias vim='vim -O'
-alias vimdiff='vim -d -c "norm ]c[c"' # jump to first difference on startup
+alias vimdiff='nvim -d -c "norm ]c[c"' # jump to first difference on startup
 alias tmux='tmux -2 -u'                        # force 256 colors AND UTF-8
-alias grep='grep --line-number --color=auto'
+alias grep='command grep --line-number --color=auto'
 alias lf='lfcd'
 alias cd='f() { c\d $1 && ls };f'
 alias drive='(){ [ ! -d "$1" ] && mkdir -p $1 && rclone --vfs-cache-mode writes mount "gdrive": ${1} && rm -r "$1" }'
+alias vim='nvim'
