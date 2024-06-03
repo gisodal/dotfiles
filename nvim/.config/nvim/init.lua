3acef8fe15c5ecea9439e8055e082bcbf867e2ed
gdal@rts <gisodal@merapar.com>
2024-06-03 20:24:18 +0000

    [neovim] Add lsp doc link

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -2,6 +2,7 @@
 require("config.lazy")
 
 -- manually add clangd
+-- see docs at https://github.com/neovim/nvim-lspconfig/blob/master/doc/lspconfig.txt
 if vim.fn.hostname() == "rts" then
   require("lspconfig").clangd.setup({})
   require("notify")("Loaded clangd")
