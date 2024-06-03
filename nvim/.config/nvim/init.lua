806c8cf0025d28186c894ad41501d3597256acd8
gdal@rts <gisodal@merapar.com>
2024-06-03 20:08:21 +0000

    [neovim] Added clangd

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,2 +1,8 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- manually add clangd
+if vim.fn.hostname() == "rts" then
+  require("lspconfig").clangd.setup({})
+  require("notify")("Loaded clangd")
+end
