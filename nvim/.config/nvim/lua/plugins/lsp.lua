81568f6b21dd1e4e9743ece188f150e042ab392b
gdal@rts <gisodal@merapar.com>
2024-06-06 10:13:38 +0200

    [neovim set clangd to utf-16]

---
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,13 @@
+return {
+  -- fix clangd offset encoding
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      setup = {
+        clangd = function(_, opts)
+          opts.capabilities.offsetEncoding = { "utf-16" }
+        end,
+      },
+    },
+  }
+}
