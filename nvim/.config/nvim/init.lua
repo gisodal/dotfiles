fde9fb50b69973b9ee3cd9158b32d5cff8b9d559
gdal@rts <gisodal@merapar.com>
2024-06-08 14:48:30 +0200

    [neovim] Add git diff view

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -4,6 +4,14 @@ require("config.lazy")
 -- manually add clangd
 -- see docs at https://github.com/neovim/nvim-lspconfig/blob/master/doc/lspconfig.txt
 if vim.fn.hostname() == "rts" then
-  require("lspconfig").clangd.setup({})
+  require("lspconfig").clangd.setup({
+	cmd = { "clangd", 
+	"--background-index",
+	"--clang-tidy",
+	"--completion-style=bundled",
+	"--cross-file-rename",
+	"--header-insertion=iwyu"
+    },
+  })
   require("notify")("Loaded clangd")
 end
