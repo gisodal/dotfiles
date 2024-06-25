68a5809249d85dbaca951b9d71c413fcce8b46a1
Giso Dal <giso.dal@merapar.com>
2024-06-25 10:18:01 +0200

    [neovim] update clangd config, don't use CR to accepts completions

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -3,17 +3,23 @@ require("config.lazy")
 
 -- manually add clangd
 -- see docs at https://github.com/neovim/nvim-lspconfig/blob/master/doc/lspconfig.txt
+-- see clangd options at : https://manpages.debian.org/experimental/clangd-18/clangd-18.1.en.html
 if vim.fn.hostname() == "rts" then
   require("lspconfig").clangd.setup({
-	cmd = { "clangd",
-	"--background-index",
-	"--clang-tidy",
-	"--completion-style=bundled",
-	"--cross-file-rename",
-	"--header-insertion=iwyu"
+	cmd = { "clangd-18",
+     "--background-index",
+     "-j=4",
+     "--query-driver=/usr/bin/**/clang-*,/bin/clang,/bin/clang++,/usr/bin/gcc,/usr/bin/*g++*,usr/bin/g++,/user/bin/c++",
+     "--clang-tidy",
+     -- "--clang-tidy-checks=*", -- depricated, use .clang-tidy
+     "--all-scopes-completion",
+     -- "--cross-file-rename", -- depricated
+     "--completion-style=bundled",
+     "--header-insertion-decorators",
+     "--header-insertion=iwyu",
+     "--pch-storage=memory",
     },
   })
-  require("notify")("Loaded clangd")
+  -- require("notify")("Loaded clangd")
 end
 
--- require("config.editorconfig")
---
+++ nvim/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -0,0 +1,13 @@
+return {
+  "hrsh7th/nvim-cmp",
+  opts = function(_, opts)
+    local cmp = require("cmp")
+    -- disable <CR> to select a completion
+    opts.mapping = vim.tbl_extend("force", opts.mapping, {
+      ["<CR>"] = function(fallback)
+          cmp.abort()
+          fallback()
+      end
+    })
+  end
+}
