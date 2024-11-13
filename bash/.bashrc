ab9074155ddde4cbf71c20e9f8c9b4d38e50c810
Giso Dal <giso.dal@merapar.com>
2024-11-13 10:34:33 +0100

    [git] added checkout-delete

--- bash/.bashrc
+++ bash/.bashrc
@@ -37,3 +37,10 @@ if [ -z "$SSH_TTY" ] &&
   [ -z "$TMUX" ]; then
   tmux
 fi
+
+# Generated for envman. Do not edit.
+[ -s "$HOME/.config/envman/load.sh" ] && source "$HOME/.config/envman/load.sh"
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
--- nvim/.config/nvim/snippets/cpp.lua
+++ nvim/.config/nvim/snippets/cpp.lua
@@ -4,6 +4,50 @@ local get_filename = function()
   return vim.fs.basename(vim.fn.expand("%"))
 end
 
+local get_defname = function()
+  local filename = vim.fs.basename(vim.fn.expand("%"))
+  return filename:upper():gsub("%.", "_")
+end
+
+local get_headername = function()
+  local filename = vim.fs.basename(vim.fn.expand("%"))
+  local base = filename:gsub("%.%w+$", "")
+  local header_extensions = { ".h", ".hpp" }
+  local current_dir = vim.fn.expand("%:p:h")
+
+  -- find in the current directory
+  for _, ext in ipairs(header_extensions) do
+    local headername = base .. ext
+    local headerpath = current_dir .. "/" .. headername
+
+    if vim.fn.glob(headerpath) ~= "" then
+      print("found header in : ", headerpath)
+      return headername
+    end
+  end
+
+  -- find it using lsp
+  local params = { textDocument = vim.lsp.util.make_text_document_params() }
+  local result = vim.lsp.buf_request_sync(0, "textDocument/references", params, 1000)
+
+  if result then
+    for _, res in pairs(result) do
+      for _, ref in pairs(res.result) do
+        local uri = ref.uri or ref.targetUri
+        local path = vim.uri_to_fname(uri)
+        for _, ext in ipairs(header_extensions) do
+          if path:match(base .. ext .. "$") then
+            print("found header with lsp : ", headerpath)
+            return vim.fn.fnamemodify(path, ":t")
+          end
+        end
+      end
+    end
+  end
+
+  return base .. ".h" -- Default to .h if no header file is found
+end
+
 return {
   s("ctrig", t("also loaded!!")),
   s(
@@ -23,11 +67,13 @@ return {
          //-----------------------------------------------------------------------------
          // Included Header Files
          //-----------------------------------------------------------------------------
+ 
+         #include "{headername}"
 
          //-----------------------------------------------------------------------------
          // global variables and defines
          //-----------------------------------------------------------------------------
-
+ 
          //-----------------------------------------------------------------------------
          // Constructor/Destructor (if not default)
          //-----------------------------------------------------------------------------
@@ -47,6 +93,7 @@ return {
       {
         name = "Giso Dal",
         filename = f(get_filename),
+        headername = f(get_headername),
         date = f(function()
           return vim.fn.strftime("%d/%m/%y")
         end, {}),
@@ -69,10 +116,10 @@ return {
         // Version History:
         // 1 - Baseline
         //-----------------------------------------------------------------------------
-
-        #ifndef {filename}
-        #define {filename}
-
+ 
+        #ifndef {defname}
+        #define {defname}
+ 
         //-----------------------------------------------------------------------------
         // Included Header Files
         //-----------------------------------------------------------------------------
@@ -80,12 +127,13 @@ return {
         //-----------------------------------------------------------------------------
         // Class Definition
         //-----------------------------------------------------------------------------
-
-        #endif // {filename}
+ 
+        #endif // {defname}
       ]],
       {
         name = "Giso Dal",
         filename = f(get_filename),
+        defname = f(get_defname),
         date = f(function()
           return vim.fn.strftime("%d/%m/%y")
         end, {}),
