c7517813b230990ccb4322add99d3c52a85b68f7
Giso Dal <giso.dal@merapar.com>
2025-01-17 21:31:25 +0100

    [nvim] add luasnip to blink.nvim autocomplete

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -6,22 +6,10 @@ return {
   -- `main` does not work at the moment
   dependencies = { "L3MON4D3/LuaSnip", version = "v2.*" },
   opts = {
-    snippets = {
-      expand = function(snippet)
-        require("luasnip").lsp_expand(snippet)
-      end,
-      active = function(filter)
-        if filter and filter.direction then
-          return require("luasnip").jumpable(filter.direction)
-        end
-        return require("luasnip").in_snippet()
-      end,
-      jump = function(direction)
-        require("luasnip").jump(direction)
-      end,
-    },
+    snippets = { preset = "luasnip" },
+    -- ensure you have the `snippets` source (enabled by default)
     sources = {
-      default = { "lsp", "path", "luasnip", "buffer" },
+      default = { "lsp", "path", "snippets", "buffer" },
     },
   },
 }
