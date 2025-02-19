19150078e30ad669fc81b506751bd905fa9fd28e
Giso Dal <giso.dal@merapar.com>
2025-02-19 09:44:15 +0100

    [media] update ssh config

--- media/usr/local/bin/check_activity.sh
+++ media/usr/local/bin/check_activity.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-SCRIPT_DIR=$(dirname "$(realpath "$0")")
-SCRIPT_NAME=$(basename "$0")
-PREFIX="[$SCRIPT_DIR/$SCRIPT_NAME]"
-
-function log() {
-  logger "$PREFIX $@"
-}
-
 if [[ -z "$PLEX_TOKEN" ]]; then
-  log "Plex authentication toke is not set. Set PLEX_TOKEN in /etc/environment"
+  logger "Plex authentication toke is not set. Set PLEX_TOKEN in /etc/environment"
   exit 1
 fi
 
@@ -17,13 +9,14 @@ fi
 PLEX_ACTIVITY=$(curl -sf http://localhost:32400/status/sessions?X-Plex-Token=$PLEX_TOKEN | grep -i 'mediacontainer.*size')
 
 if [ $? -ne 0 ]; then
-  log "Failed to reach plex server from check_activity.sh"
+  logger "Failed to reach plex server from check_activity.sh"
   exit 1
 fi
 
 ACTIVE=
-log "Plex state response: $PLEX_ACTIVITY"
+logger "Plex state response: $PLEX_ACTIVITY"
 if [[ "$PLEX_ACTIVITY" != *'size="0"'* ]]; then
+  echo "plex is active"
   ACTIVE="$ACTIVE plex"
 fi
 
@@ -33,17 +26,17 @@ if [[ -n "$SSH_CONNECTIONS" ]]; then
   ACTIVE="$ACTIVE ssh"
 fi
 
-log "Server activity with:$ACTIVE"
+logger "Server activity with:$ACTIVE"
 if [[ -z $ACTIVE ]]; then
   # No active streams, schedule shutdown
   if [ ! -f /run/systemd/shutdown/scheduled ]; then
-    log "No activity, shutting down in 5 minutes."
+    logger "No activity, shutting down in 5 minutes."
     sudo shutdown -h +5 >/dev/null 2>&1
   else
-    log "No activity, shutting down timer active."
+    logger "No activity, shutting down timer active."
   fi
 elif [ -f /run/systemd/shutdown/scheduled ]; then
   # Active streams, cancel any pending shutdown
-  log "Active Plex streams, cancel shutdown."
+  logger "Active Plex streams, cancel shutdown."
   sudo shutdown -c
 fi
