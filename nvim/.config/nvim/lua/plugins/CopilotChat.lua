d08b72f85de9627d10ae66cd42320d28cf085e68
Giso Dal <giso.dal@merapar.com>
2025-03-08 23:13:02 +0100

    [nvim] set copilot to claude-3.7

--- nvim/.config/nvim/lua/plugins/CopilotChat.lua
+++ nvim/.config/nvim/lua/plugins/CopilotChat.lua
@@ -1,7 +1,17 @@
 return {
-  "CopilotC-Nvim/CopilotChat.nvim",
-  opts = {
-    answer_header = "  Copilot (claude-3.5-sonnet) ",
-    model = "claude-3.5-sonnet",
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      { "zbirenbaum/copilot.lua" }, -- or zbirenbaum/copilot.lua
+      { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+    },
+    build = "make tiktoken", -- Only on MacOS or Linux
+    opts = {
+      answer_header = "  Copilot (claude-3.7-sonnet) ",
+      model = "claude-3.7-sonnet",
+
+      -- See Configuration section for options
+    },
+    -- See Commands section for default commands if you want to lazy load on them
   },
 }
