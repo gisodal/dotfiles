d471d02f5cb4c954b6964be0bb173d42db5ae60b
Giso Dal <giso.dal@merapar.com>
2025-05-15 14:37:34 +0200

    feat(nvim): add copilot toggle to nvim

--- config/nvim/.config/nvim/lua/config/autocmds.lua
+++ config/nvim/.config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,26 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+local Snacks = require("snacks")
+local copilot_exists = pcall(require, "copilot")
+
+if copilot_exists then
+  Snacks.toggle({
+    name = "Copilot Completion",
+    color = {
+      enabled = "azure",
+      disabled = "orange",
+    },
+    get = function()
+      return not require("copilot.client").is_disabled()
+    end,
+    set = function(state)
+      if state then
+        require("copilot.command").enable()
+      else
+        require("copilot.command").disable()
+      end
+    end,
+  }):map("<leader>at")
+end
