37b516295ea164b8ab425fa4713e4baf0be9b53d
Giso Dal <giso.dal@merapar.com>
2025-10-02 14:33:37 +0200

    [zsh] fix zsh fzf keybindings in vim insert mode

--- config/zsh/.config/zsh/environment.sh
+++ config/zsh/.config/zsh/environment.sh
@@ -7,6 +7,15 @@ export BROWSER="google-chrome-stable"
 # setup fzf
 if command -v fzf 2>/dev/null 1>&2; then
 	source <(fzf --zsh)
+
+	# Fix fzf key bindings for zsh-vi-mode
+	function zvm_after_init() {
+		[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+		# Restore fzf key bindings
+		bindkey '^R' fzf-history-widget
+		bindkey '^T' fzf-file-widget
+		bindkey '\ec' fzf-cd-widget
+	}
 fi
 
 #setup direnv if it is available
