4e281f7a22ff1ec6a3693f67bb0b02e7d60cc79f
Giso Dal <giso.dal@merapar.com>
2026-03-31 23:49:53 +0200

    add async call to git prompt

--- config/zsh/.config/zsh/prompt.sh
+++ config/zsh/.config/zsh/prompt.sh
@@ -5,25 +5,61 @@
 # -----------------------------------------------------------------------------
 
 function prompt_hostname() {
-  if [[ -z "$TMUX" ]]; then
-    print -n "$(tput setaf 5)$(hostname)$(tput sgr0) "
-  fi
+  [[ -z "$TMUX" ]] && print -n '\033[35m%m\033[0m '
 }
 
 function prompt_exit_code() {
-  [ $1 -ne 0 ] && print -n "%K{160}%B $1 %b%k "
+  [[ $1 -ne 0 ]] && print -n "%K{160}%B $1 %b%k "
 }
 
 function prompt_location() {
-  print -n "$(tput setaf 2)%(5~|.../%3~|%~)$(tput sgr0)"
+  print -n '\033[32m%(5~|.../%3~|%~)\033[0m'
 }
 
-# Set directory trimming
-export PROMPT_DIRTRIM=2
+# -----------------------------------------------------------------------------
+# async git info
+# -----------------------------------------------------------------------------
 
-# Set up prompts
+typeset -g _prompt_last_exit=0
+typeset -g _prompt_git_info=""
+typeset -g _prompt_git_fd=
+
+function _prompt_git_callback() {
+  local fd=$1
+  {
+    IFS= read -r -u "$fd" _prompt_git_info || true
+  } always {
+    zle -F "$fd"
+    exec {fd}<&-
+    _prompt_git_fd=
+  }
+  zle && zle reset-prompt
+}
+
+function _prompt_precmd() {
+  _prompt_last_exit=$?
+
+  # close previous async worker if still running
+  if [[ -n "$_prompt_git_fd" ]]; then
+    zle -F "$_prompt_git_fd" 2>/dev/null
+    exec {_prompt_git_fd}<&- 2>/dev/null
+  fi
+
+  # start background worker — result arrives via _prompt_git_callback
+  exec {_prompt_git_fd}< <(prompt_git_info)
+  zle -F "$_prompt_git_fd" _prompt_git_callback
+}
+
+autoload -Uz add-zsh-hook
+add-zsh-hook precmd _prompt_precmd
+
+# -----------------------------------------------------------------------------
+# prompt string
+# -----------------------------------------------------------------------------
+
+export PROMPT_DIRTRIM=2
 setopt PROMPT_SUBST
 export PROMPT='
-$(prompt_exit_code $?)$(prompt_hostname)$(prompt_location) $(prompt_git_info)
+$(prompt_exit_code $_prompt_last_exit)$(prompt_hostname)$(prompt_location) ${_prompt_git_info}
 > '
 export PROMPT2='> '
